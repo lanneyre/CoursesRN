69dc495576c5b00482a38f49fcd0e674dbe0e95e
Rémi LANNEY RICCI <r.lanney@ics-nice.com>
2022-01-19 14:31:47 +0100

    COmponent de chargement

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, ActivityIndicator } from 'react-native'
 import Listing from './Listing'
 import l from '../Data/liste.json'
 
@@ -8,42 +8,59 @@ class Search extends React.Component {
      super(props)
      // Ici on va créer les propriétés de notre component custom Search
      this.state = { 
-       liste: l,
-       search:""
+       liste: l, // contient toute la liste de course
+       search:"", // contiendra le text recherché 
+       isLoading: false // Permet de savoir si on cherche un élément ou pas
       }
 
    }
-
-   //test
+  _loadListe(){
+    const newList = [];
+    this.setState({ liste: l }, () => {
+      this.setState({ isLoading: true }) // Lancement du chargement
+      if(this.state.search.length > 2 ){
+        this.state.liste.forEach(element => {
+          if(element.name.includes(this.state.search)){
+            newList.push(element);
+          }
+        })
+        this.setState({ liste: newList, isLoading:false });
+      } else if(this.state.search != "") {
+        alert("Recherche trop courte")
+      }
+    });
+  }
+  _displayLoading() {
+    if (this.state.isLoading) {
+      console.log("Load in progress")
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
+    }
+  }
   
   render() {
     return (
       <View style={styles.main}>
         <View style={styles.main_container}>
-          <TextInput placeholder='Recherche' style={styles.textinput} onChangeText={(text)=>{
-            this.setState({ search: text },()=>{
-              console.log(this.state.search)
-            });
-          }} />
+          <TextInput 
+            placeholder='Recherche' 
+            style={styles.textinput} 
+            onChangeText={(text)=>{
+              this.setState({ search: text },()=>{
+                console.log(this.state.search)
+              });
+            }}
+            onSubmitEditing={() => this._loadListe()} />
           <View style={styles.btn}>
-              <Button title='Rechercher' onPress={() => {
-                const newList = [];
-                this.setState({ liste: l });
-                if(this.state.search.length > 2 ){
-                  this.state.liste.forEach(element => {
-                    if(element.name.includes(this.state.search)){
-                      newList.push(element);
-                    }
-                  })
-                  this.setState({ liste: newList });
-                } else {
-                  alert("Recherche trop courte")
-                }
-                
-                
-              }} />
-          </View>
+              <Button title='Rechercher' onPress={() => this._loadListe()} />
+          </View> 
         </View>
+
+        {this._displayLoading()}
         <Listing liste={this.state.liste}/>
         
         
@@ -74,6 +91,15 @@ const styles = StyleSheet.create({
     btn:{
         marginBottom: 30,
         padding: 5
+    }, 
+    loading_container: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 100,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center'
     }
 })
 
